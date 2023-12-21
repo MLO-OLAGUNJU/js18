74775004415f378ad91bdd1d56938939eebb3d4c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-21 11:02:01 +0100

    JSON-JavaScript Object Notation

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ JSON is a text format that is completely language independent.
 Meaning JSON is used to send & receive data in many languages
 ...not just in Javascript.
 */
+
+const myObj = {
+  myName: "Manny",
+  hobbies: ["eat", "sleep", "code"],
+  hello: function () {
+    console.log("Hello!");
+  },
+};
+
+console.log(myObj);
+console.log(myObj.myName);
+myObj.hello();
